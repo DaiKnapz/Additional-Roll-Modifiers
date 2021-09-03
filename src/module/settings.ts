d119c12b3f6f895e89d21cb8109f9724d5cbc842
David Knapman <dai.knapz@gmail.com>
2021-09-03 13:28:34 +0100

    Added settings config

--- src/module/settings.ts
+++ src/module/settings.ts
@@ -1,3 +1,19 @@
+import { MODULE_NAMESPACE } from "../types/Constants";
+
 export function registerSettings(): void {
-	// Register any custom module settings here
+	const replaceRollSetting: ClientSettings.PartialSetting<boolean> = {
+    	config: true,
+		type: Boolean,
+    	default: true,
+    	hint: "Modifier to replace rolls with a flat value when the result matches certain criteria. e.g. /r 10d4rep=1,9 rolls 10 d4s and replaces any 1s with a 9",
+    	name: "Relace roll",
+    	onChange: (value) => {
+			console.log(`Replace roll setting changed, new value${value}`)
+		},
+    	scope: "world",
+	};
+
+	if (game instanceof Game){
+		game.settings.register(MODULE_NAMESPACE, "ReplaceRoll", replaceRollSetting);
+	}
 }
---
+++ src/types/Constants.ts
@@ -0,0 +1,1 @@
+export const MODULE_NAMESPACE = 'Additional-Roll-Modifiers'
