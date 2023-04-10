18d6f42b747c8179e191dc917146f267e39526f2
David Knapman <dai.knapz@gmail.com>
2023-04-10 20:57:52 +0100

    Fixed test types

--- test/modifiers.test.ts
+++ test/modifiers.test.ts
@@ -1,50 +1,53 @@
-import { AdditionalModifiersResult } from "../src/types/DiceTerm";
-import { registerModifiers, replace, replaceFunction } from "../src/module/modifiers";
-import { MODULE_NAMESPACE } from "../src/types/Constants";
+import { AdditionalModifiersResult } from '../src/types/DiceTerm';
+import { registerModifiers, replace, replaceFunction } from '../src/module/modifiers';
+import { MODULE_NAMESPACE } from '../src/types/Constants';
 
-describe("Modifiers", () => {
-	describe("Replace", () => {
+describe('Modifiers', () => {
+	describe('Replace', () => {
 		let compareResult = jest.fn();
 		let results: AdditionalModifiersResult[] = [];
 
 		beforeEach(() => {
 			results = [];
-			compareResult = jest.fn()
+			compareResult = jest.fn();
 		});
 
-		it("No modifier - Returns when no match is found", () => {
+		it('No modifier - Returns when no match is found', () => {
 			// Arrange
-			const modifier = "";
-			results = [{
-				result: 0
-			}];
+			const modifier = '';
+			results = [
+				{
+					result: 0,
+				},
+			];
 
 			// Should not get to comparing results
 			compareResult.mockReturnValue(() => {
-				throw "Whoops";
+				throw 'Whoops';
 			});
 
 			// Act
-			replaceFunction(results, modifier, compareResult)
+			replaceFunction(results, modifier, compareResult);
 
 			// Assert
 			expect(results[0].replaced).toBeFalsy();
 		});
 
-		it("Simple Replace - No equals", () => {
+		it('Simple Replace - No equals', () => {
 			// Arrange
-			const modifier = "1d6rep5,10";
-			results = [{
-				result: 5
-			},
-			{
-				result: 4
-			}];
+			const modifier = '1d6rep5,10';
+			results = [
+				{
+					result: 5,
+				},
+				{
+					result: 4,
+				},
+			];
 
-			compareResult.mockImplementation(((result: number, comparison: string, target: number) => {
+			compareResult.mockImplementation((result: number, comparison: string, target: number) => {
 				return result == 5;
-			}));
-
+			});
 
 			// Act
 			replaceFunction(results, modifier, compareResult);
@@ -55,21 +58,22 @@ describe("Modifiers", () => {
 			expect(results[1].replaced).toBeFalsy();
 			expect(results[1].result).toBe(4);
 			expect(compareResult).toHaveBeenCalledTimes(2);
-			expect(compareResult).toHaveBeenNthCalledWith(1, 5, "=", 5);
-			expect(compareResult).toHaveBeenNthCalledWith(2, 4, "=", 5);
+			expect(compareResult).toHaveBeenNthCalledWith(1, 5, '=', 5);
+			expect(compareResult).toHaveBeenNthCalledWith(2, 4, '=', 5);
 		});
 
-		it("Simple replace, passes in comparison", () => {
+		it('Simple replace, passes in comparison', () => {
 			// Arrange
-			const modifier = "1d6rep<=3,10";
-			results = [{
-				result: 2
-			}];
+			const modifier = '1d6rep<=3,10';
+			results = [
+				{
+					result: 2,
+				},
+			];
 
-			compareResult.mockImplementation(((result: number, comparison: string, target: number) => {
+			compareResult.mockImplementation((result: number, comparison: string, target: number) => {
 				return result <= 3;
-			}));
-
+			});
 
 			// Act
 			replaceFunction(results, modifier, compareResult);
@@ -78,74 +82,74 @@ describe("Modifiers", () => {
 			expect(results[0].replaced).toBeTruthy();
 			expect(results[0].result).toBe(10);
 			expect(compareResult).toHaveBeenCalledTimes(1);
-			expect(compareResult).toHaveBeenNthCalledWith(1, 2, "<=", 3);
+			expect(compareResult).toHaveBeenNthCalledWith(1, 2, '<=', 3);
 		});
 
-		it("No Replace", () => {
+		it('No Replace', () => {
 			// Arrange
-			const modifier = "1d6rep5,10";
-			results = [{
-				result: 4
-			}];
+			const modifier = '1d6rep5,10';
+			results = [
+				{
+					result: 4,
+				},
+			];
 
 			compareResult.mockReturnValue(false);
 
 			// Act
-			replaceFunction(results, modifier, compareResult)
+			replaceFunction(results, modifier, compareResult);
 
 			// Assert
 			expect(results[0].replaced).toBeFalsy();
 			expect(results[0].result).toBe(4);
 		});
-	})
+	});
 
-	describe("Register Modules", () => {
+	describe('Register Modules', () => {
 		let mockDie: any;
-		let mockGame = {
+		const mockGame = {
 			settings: {
 				get: jest.fn(),
-				set: jest.fn(), 
-			}
-		}
+				set: jest.fn(),
+			},
+		};
 
 		beforeEach(() => {
 			mockDie = {
-				r: "reroll"
-			}
-		})
+				r: 'reroll',
+			};
+		});
 
-		it("No modifiers enabled", () => {
+		it('No modifiers enabled', () => {
 			// Arrange
 			mockGame.settings.get.mockReturnValue(false);
 
 			// Act
-			registerModifiers(mockDie, mockGame);
+			registerModifiers(mockDie, mockGame as unknown as Game);
 
 			// Assert
 			expect(mockDie).toEqual({
-				r: "reroll"
+				r: 'reroll',
 			});
-		})
+		});
 
-		it("Replace modifier enabled", () => {
+		it('Replace modifier enabled', () => {
 			// Arrange
-			mockGame.settings.get.mockImplementation(
-				(module: string, settingName: string) =>{
-					if (module == MODULE_NAMESPACE && settingName == "ReplaceRoll") {
-						return true;
-					}
-					return false;
+			mockGame.settings.get.mockImplementation((module: string, settingName: string) => {
+				if (module == MODULE_NAMESPACE && settingName == 'ReplaceRoll') {
+					return true;
 				}
-			)
+				return false;
+			});
 
 			// Act
-			registerModifiers(mockDie, mockGame);
+			registerModifiers(mockDie, mockGame as unknown as Game);
 
 			// Assert
 			expect(mockDie).toEqual({
-				r: "reroll",
-				rep: replace
+				r: 'reroll',
+				rep: replace,
 			});
-		})
-	})
+		});
+	});
 });
